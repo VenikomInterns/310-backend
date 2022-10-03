3116a2927c5eb6f72273e272034f152084262625
VenikomInterns <114774928+VenikomInterns@users.noreply.github.com>
2022-10-04 01:07:01 +0200

    api review

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -17,10 +17,12 @@ class ProductApiController extends Controller
             $filtered = Product::query()->paginate(10);
         }
         else {
+            // fetching all products and then filtering in memory?
             $filtered = Product::all()->filter(function ($item) use ($compare) {
                 return $item->category_id == $compare;
             });
         }
+        // sometimes we return paginate response, otherwise we return all items.
 
         return JsonResource::collection($filtered);
 
